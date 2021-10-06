3ca947046eb91f77400fa8208b6788c1e168f65c
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-10-06 12:55:38 -0700

    s

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -34,7 +34,19 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ) {
+    next({
+      status: 400, 
+      message: 'invalid schemme_name'
+    })
+  } else {
+    next()
+  }
 }
 
 /*
@@ -47,7 +59,22 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  const { instructions, step_number } = req.body
+  if (
+    instructions === undefined || 
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ) {
+    const error = {
+      status: 400,
+      message: 'invalid step'
+    }
+    next(error)
+  } else {
+    next()
+  }
 }
 
 module.exports = {
